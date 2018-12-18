bb92d2709dd299d0770f7ac58cd3116803c36271
David Mihal <david@dmihal.com>
2018-12-18 21:09:26 +0700

    Create card grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
 import FunctionSignature from './cards/FunctionSignature';
 import TransactionBuilder from './cards/TransactionBuilder';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TransactionBuilder />
-        <FunctionSignature />
-      </div>
-    );
-  }
-}
+const styles = {
+  app: {
+    margin: '16px',
+  },
+};
 
-export default App;
+const App = ({ classes }: { classes: any }) => (
+  <div className={classes.app}>
+    <Grid container spacing={16}>
+      <Grid item md={8}>
+        <TransactionBuilder />
+      </Grid>
+      <Grid item md={4}>
+        <FunctionSignature />
+      </Grid>
+    </Grid>
+  </div>
+);
+
+export default withStyles(styles)(App);
